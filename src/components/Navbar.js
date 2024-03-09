eae773b0df56a85ae4f402b320f74e702a7d2b99
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-03-08 22:11:43 -0500

    created navbar and added routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Navbar = () => {
+return (
+    <nav className="bg-blue-500 text-white py-4">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-4">
+            {/* Logo or Brand name */}
+            <div>
+              <a href="#" className="flex items-center text-white py-2 px-3">
+                <span className="font-bold">My Fitness Tracker</span>
+              </a>
+            </div>
+            {/* Primary Nav */}
+            <div className="hidden md:flex items-center space-x-1">
+              <a href="#" className="py-2 px-3 text-white rounded hover:bg-blue-700 transition duration-300">Home</a>
+              <a href="#" className="py-2 px-3 text-white rounded hover:bg-blue-700 transition duration-300">About</a>
+              <a href="#" className="py-2 px-3 text-white rounded hover:bg-blue-700 transition duration-300">Services</a>
+              <a href="#" className="py-2 px-3 text-white rounded hover:bg-blue-700 transition duration-300">Contact</a>
+            </div>
+          </div>
+          {/* Secondary Nav */}
+          <div className="hidden md:flex items-center space-x-1">
+            <a href="#" className="py-2 px-3 text-white rounded hover:bg-blue-700 transition duration-300">Login</a>
+            <a href="#" className="py-2 px-3 text-white bg-blue-700 hover:bg-blue-800 rounded transition duration-300">Sign Up</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
