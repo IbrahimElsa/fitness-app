97b31eddc7f1f17bc327f43c624ee33283ce257c
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-03-13 20:54:44 -0400

    Initialized firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDPVV_dMvLLgKJ2w_xzqUTODeh6YWYC3fk",
+    authDomain: "fitness-app-00.firebaseapp.com",
+    databaseURL: "https://fitness-app-00-default-rtdb.firebaseio.com",
+    projectId: "fitness-app-00",
+    storageBucket: "fitness-app-00.appspot.com",
+    messagingSenderId: "272840826228",
+    appId: "1:272840826228:web:45e8a3f04594303c36a021"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
