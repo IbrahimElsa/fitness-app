fe517631d0be58076ce19551d04763432d772581
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-03-27 22:52:21 -0400

    Update Firebase configuration and save workout data

--- src/pages/ActiveWorkout.js
+++ src/pages/ActiveWorkout.js
@@ -4,7 +4,9 @@ import ActiveWorkoutModal from "../components/ActiveWorkoutModal";
 import CancelModal from "../components/CancelModal";
 import exercisesData from "../components/Exercises.json";
 import MobileNavbar from "../components/MobileNavbar";
-import ExerciseSet from "../components/ExerciseSet"; // Import the ExerciseSet component
+import ExerciseSet from "../components/ExerciseSet"; 
+import { db } from '../firebaseConfig';
+import { collection, addDoc } from "firebase/firestore";
 
 function ActiveWorkout() {
   const [workoutExercises, setWorkoutExercises] = useState([]);
@@ -21,8 +23,21 @@ function ActiveWorkout() {
     setShowModal(false);
   };
 
-  const handleFinishWorkout = () => {
-    navigate("/");
+  const handleFinishWorkout = async () => {
+    const userId = 'current-user-id';
+
+    try {
+      
+      await addDoc(collection(db, "workouts"), {
+        userId: userId,
+        exercises: workoutExercises,
+        timestamp: new Date(), // Store the date and time of workout completion
+      });
+      console.log("Workout saved successfully!");
+      navigate("/");
+    } catch (error) {
+      console.error("Error saving workout: ", error);
+    }
   };
 
   const handleCancelWorkout = () => {
@@ -39,7 +54,6 @@ function ActiveWorkout() {
 
   return (
     <div className="active-workout-page bg-gray-900 text-white min-h-screen">
-      {/* Timer and workout note components should go here */}
       <div className="flex flex-col items-center pt-4 space-y-4">
         <button
           className="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:outline-none rounded text-white"
@@ -65,26 +79,10 @@ function ActiveWorkout() {
           show={showModal}
           onClose={() => setShowModal(false)}
           title="Add Exercise"
-          className="z-50"
           exercisesData={exercisesData}
           handleAddExercise={handleAddExercise}
-        >
-          <div className="max-h-96 overflow-y-auto">
-            <ul className="flex flex-col space-y-2">
-              {exercisesData.Exercises.map((exercise, index) => (
-                <li
-                  key={index}
-                  onClick={() => handleAddExercise(exercise)}
-                  className="cursor-pointer bg-gray-200 text-gray-800 rounded-md p-4 hover:bg-gray-300 transition-colors duration-200"
-                >
-                  {exercise.Name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </ActiveWorkoutModal>
+        />
       )}
-      {/* List of added exercises */}
       <div className="exercises">
         {workoutExercises.map((exercise, exerciseIndex) => (
           <div key={exerciseIndex}>
