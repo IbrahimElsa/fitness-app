8eb477f7991f057e046925f7c8c3bd9f35aa9adf
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-03-29 22:45:02 -0400

    Update Coach and History pages

--- src/pages/Coach.js
+++ src/pages/Coach.js
@@ -1,12 +1,15 @@
 import React from "react";
 import MobileNavbar from "../components/MobileNavbar";
 import Navbar from "../components/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faScrewdriverWrench } from "@fortawesome/free-solid-svg-icons";
 
 function CoachPage() {
 return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
         <Navbar />
-        <h1 className="text-3xl ">Coach</h1>
+            <FontAwesomeIcon icon={faScrewdriverWrench} size="4x"/>
+            <h1 className="text-3xl pt-5">Coach Page WIP</h1>
         <MobileNavbar />
     </div>
 );}
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,14 +1,17 @@
 import React from "react";
 import MobileNavbar from "../components/MobileNavbar";
 import Navbar from "../components/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faScrewdriverWrench } from "@fortawesome/free-solid-svg-icons";
 
 function HistoryPage() {
-return (
-    <div>
-        <Navbar />
-        <h1 className="text-3xl ">History</h1>
-        <MobileNavbar />
-    </div>
-);}
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <Navbar />
+                <FontAwesomeIcon icon={faScrewdriverWrench} size="4x"/>
+                <h1 className="text-3xl pt-5">History Page WIP</h1>
+            <MobileNavbar />
+        </div>
+    );}
 
 export default HistoryPage;
