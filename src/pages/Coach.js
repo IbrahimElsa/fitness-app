ef1d168a3d9003853ae5bf2a82535bd1d5dfd5ec
Ibrahim Elsawalhi <ielsawalhi@usf.edu>
2024-04-13 23:48:44 -0400

    added light/dark mode to coach & history pages

--- src/pages/Coach.js
+++ src/pages/Coach.js
@@ -3,14 +3,21 @@ import MobileNavbar from "../components/MobileNavbar";
 import Navbar from "../components/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faScrewdriverWrench } from "@fortawesome/free-solid-svg-icons";
+import { useTheme } from "../context/ThemeContext";
 
 function CoachPage() {
+    const { theme } = useTheme();
+
+    const containerClass = theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white';
+
 return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-        <Navbar />
-            <FontAwesomeIcon icon={faScrewdriverWrench} size="4x"/>
-            <h1 className="text-3xl pt-5">Coach Page WIP</h1>
-        <MobileNavbar />
+    <div className={`coach-page min-h-screen ${containerClass}`}>
+        <div className={'coach-page flex flex-col items-center justify-center min-h-screen'}>
+            <Navbar />
+                <FontAwesomeIcon icon={faScrewdriverWrench} size="4x"/>
+                <h1 className="text-3xl pt-5">Coach Page WIP</h1>
+            <MobileNavbar />
+        </div>
     </div>
 );}
 
--- src/pages/History.js
+++ src/pages/History.js
@@ -3,14 +3,21 @@ import MobileNavbar from "../components/MobileNavbar";
 import Navbar from "../components/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faScrewdriverWrench } from "@fortawesome/free-solid-svg-icons";
+import { useTheme } from "../context/ThemeContext";
 
 function HistoryPage() {
+const { theme } = useTheme();
+
+const containerClass = theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white';
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <Navbar />
-                <FontAwesomeIcon icon={faScrewdriverWrench} size="4x"/>
-                <h1 className="text-3xl pt-5">History Page WIP</h1>
-            <MobileNavbar />
+        <div className={`history-page min-h-screen ${containerClass}`}>
+            <div className="flex flex-col items-center justify-center">
+                <Navbar />
+                    <FontAwesomeIcon icon={faScrewdriverWrench} size="4x"/>
+                    <h1 className="text-3xl pt-5">History Page WIP</h1>
+                <MobileNavbar />
+            </div>
         </div>
     );}
 
